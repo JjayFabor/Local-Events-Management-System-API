e0ada9def29e69dd00ce40dfea5150ffaad9a238
JjayFabor <faborjaylordvhan@gmail.com>
2024-08-01 22:22:38 +0800

    Added github page domain

--- api/settings.py
+++ api/settings.py
@@ -35,6 +35,7 @@ AUTH_USER_MODEL = "users.CustomUser"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://JjayFabor.github.io",
 ]
 # Application definition
 
