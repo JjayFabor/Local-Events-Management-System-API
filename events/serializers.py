80482e8fffa098c1c43e9f21827410b195bdcf36
JjayFabor <faborjaylordvhan@gmail.com>
2024-08-02 10:15:26 +0800

    Added ErrorSerializer

--- events/serializers.py
+++ events/serializers.py
@@ -6,10 +6,14 @@ from django.utils.translation import gettext_lazy as _
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["name"]
+        fields = ["id", "name"]
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventModel
         fields = "__all__"
+
+
+class ErrorSerializer(serializers.Serializer):
+    error = serializers.CharField()
