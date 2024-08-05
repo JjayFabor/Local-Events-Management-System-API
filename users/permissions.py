38e36e549e3cf76b02b92f404884b1873fe45a71
Jaylord Vhan Fabor <82490827+JjayFabor@users.noreply.github.com>
2024-08-05 16:55:59 +0800

    Delete users/permissions.py

--- users/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class isGovernmentAuthority(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user
-            and request.user.groups.filter(name="Government Authority").exists()
-        )
